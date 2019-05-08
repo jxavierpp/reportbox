4f8a708ae795c04861c8a8aa16ff2829084e45a7
Xavier Paredes <jparedes@quantumbit.mx>
2019-05-08 10:40:14 -0700

    init clases, los modelos controladores migraciones

---
+++ app/Http/Controllers/EvidencyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Evidency;
+use Illuminate\Http\Request;
+
+class EvidencyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Evidency  $evidency
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Evidency $evidency)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Evidency  $evidency
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Evidency $evidency)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Evidency  $evidency
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Evidency $evidency)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Evidency  $evidency
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Evidency $evidency)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/RegistryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\registry;
+use Illuminate\Http\Request;
+
+class RegistryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\registry  $registry
+     * @return \Illuminate\Http\Response
+     */
+    public function show(registry $registry)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\registry  $registry
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(registry $registry)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\registry  $registry
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, registry $registry)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\registry  $registry
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(registry $registry)
+    {
+        //
+    }
+}
