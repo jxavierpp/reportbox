9cc9012b572f6e0e2f4f2fd6a19b343d2b0cbb20
Miguel Romero <miguelrom27@gmail.com>
2019-05-13 13:09:22 -0700

    Categorias por default en la migracion

--- database/migrations/2019_05_08_173601_create_categories_table.php
+++ database/migrations/2019_05_08_173601_create_categories_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateCategoriesTable extends Migration
 {
@@ -16,11 +17,25 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name', 100);
-            $table->bigInteger('encargado')->unsigned();
+            $table->bigInteger('encargado')->unsigned()->nullable();
             $table->foreign('encargado')->references('id')->on('users');
             $table->timestamps();
         });
-    }
+
+        DB::table('categories')->insert([
+            ['name' => 'Personal Academico'],
+            ['name' => 'Estudiantes'],
+            ['name' => 'Plan de Estudios'],
+            ['name' => 'Evaluacion del Aprendizaje'],
+            ['name' => 'Formacion Integral'],
+            ['name' => 'Servicion de Apoyo para el Aprendizaje'],
+            ['name' => 'Vinculacion_Extension'],
+            ['name' => 'Investigacion'],
+            ['name' => 'Infraestrutura y Equipamiento'],
+            ['name' => 'Gestion Administrativa y Financiacion']
+        ]);
+
+    }//End method up
 
     /**
      * Reverse the migrations.
