e1c578e2f79a48d8159c0eaa202fafd58674852a
Xavier Paredes <jparedes@quantumbit.mx>
2019-05-13 12:28:33 -0700

    administrador por default

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\User;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Hash;
 
 class CreateUsersTable extends Migration
 {
@@ -17,12 +19,17 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('rol')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
+        $admin_default = new User();
+        $admin_default->name = "admin";
+        $admin_default->email = "admin@admin.com";
+        $admin_default->password = Hash::make("102030");
+        $admin_default->rol = 0;
+        $admin_default->save();
     }
 
     /**
