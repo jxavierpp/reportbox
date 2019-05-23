d28d72d426d5ba50ca907af89121f0c3040999e2
Xavier Paredes <jparedes@quantumbit.mx>
2019-05-23 02:25:09 -0700

    formato de fechas

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'es',
 
     /*
     |--------------------------------------------------------------------------
@@ -230,6 +230,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'PDF' => Barryvdh\domPDF\Facade::class,
+        'Carbon' => 'Carbon\Carbon',
 
     ],
 
